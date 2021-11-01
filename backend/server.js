5d8ecae0c0e62afac879dbce357a95798eed5cbd
ajay <ajaynairarun@gmail.com>
2021-11-01 10:25:55 +0530

    add connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,64 @@
 const express = require("express");
+let { Server } = require("socket.io");
 const app = express();
 const cors = require("cors");
-const port = 3000;
+
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use(cors());
-
-app.listen(port, () => {
+let server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const io = new Server(server, { cors: { origin: "*" } });
+
+//initializing the socket io connection
+io.on("connection", (socket) => {
+  //for a new user joining the room
+  socket.on("joinRoom", ({ username, roomname }) => {
+    //* create user
+    console.log(username, roomname);
+    const p_user = join_User(socket.id, username, roomname);
+    console.log(socket.id, "=id");
+    socket.join(p_user.room);
+
+    //display a welcome message to the user who have joined a room
+    socket.emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `Welcome ${p_user.username}`,
+    });
+
+    //displays a joined room message to all other room users except that particular user
+    socket.broadcast.to(p_user.room).emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `${p_user.username} has joined the chat`,
+    });
+  });
+
+  //when the user exits the room
+  socket.on("disconnect", () => {
+    // //the user is deleted from array of users and a left room message displayed
+    // const p_user = user_Disconnect(socket.id);
+    console.log("disconnected");
+    // if (p_user) {
+    //   io.to(p_user.room).emit("message", {
+    //     userId: p_user.id,
+    //     username: p_user.username,
+    //     text: `${p_user.username} has left the room`,
+    //   });
+    // }
+  });
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,24 @@
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(`http://localhost:5000`);
+    console.log(newSocket)
+    return () => newSocket.close();
+  }, [setSocket]);
+
+  const sendData = (username,roomname) => {
+    if (username !== "" && roomname !== "") {
+      socket.emit("joinRoom", { username: "ajay", roomname: "1" });
+      //if empty error message pops up and returns to the same page
+    } else {
+      alert("username and roomname are must !");
+      window.location.reload();
+    }
+  };
   return (
     <div className="App">
       <p>Hi</p>
